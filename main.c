5f66073e84b6a8b965c4a765c5a2af813b783864
Łukasz Tulikowski <lukasz@tulik.info>
2014-10-07 15:10:27 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-#include "calnum.h"		/* tu naglowek funkcji calkujacej */
-
+#include "calnum.h"		
 double 
 f(double x)
 {
@@ -12,12 +11,11 @@ f(double x)
 int
 main(int argc, char **argv)
 {
-	double		a = argc > 1 ? atof(argv[1]) : 0;	/* pocz. przedzialu
-								 * calkowania */
-	double		b = argc > 2 ? atof(argv[2]) : M_PI;	/* koniec przedzialu
-								 * calkowania */
-	int		n = argc > 3 ? atoi(argv[3]) : 10000;	/* liczba krokow
-								 * calkowania */
+	double		a = argc > 1 ? atof(argv[1]) : 0;	// beginning of the integration interval
+								
+	double		b = argc > 2 ? atof(argv[2]) : M_PI;	// end of the integration interval	
+ 
+	int		n = argc > 3 ? atoi(argv[3]) : 10000;	// number of steps of integration
 
 	printf("Int <%g,%g>[%i] (5*x)= %g\n", a, b, n, calnum(f, a, b, n));
 
